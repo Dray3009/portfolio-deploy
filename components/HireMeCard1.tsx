5122e376bb107de5efa193a06614e85e0556c09f
Dray3009 <andrevassell89@gmail.com>
2023-03-07 18:26:31 +0000

    first commit

--- components/HireMeCard1.tsx
+++ components/HireMeCard1.tsx
@@ -41,7 +41,7 @@ const HireMeCard1 = (props: Props) => {
             <ul className='list-disc space-y-4 ml-5 text-lg mt-2'>
                 <li> Continue learning </li>
                 <li> growth mindset</li>
-                <li> Summmary summary summary summary</li>
+                <li> continous devlopment</li>
                 <li> Summmary summary summary summary</li>
             </ul>
 
